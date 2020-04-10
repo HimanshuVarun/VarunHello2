486258d1ee5781bdf8e463f7d929862a5b584aad
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-10 22:31:12 +0530

    first commit

---
+++ src/main/java/Adminui/Adminui/App.java
@@ -0,0 +1,13 @@
+package Adminui.Adminui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Adminui/Adminui/Addd.java
@@ -0,0 +1,21 @@
+package Adminui.Adminui;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Addd 
+{
+	 @BeforeTest
+	 public void mm()
+	 {
+		 System.out.println("Hello varun");
+	 }
+	 @Test
+	 public void rr()
+	 {
+		 System.out.println(" varun Hello");
+
+	 }
+
+
+}
